16b335a3b7b67700911de52b5f9c4bd61138e4e6
Lucas <lucasdbs@ext.inditex.com>
2022-09-13 10:44:23 +0200

    scaffolding

---
+++ code/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './views/home/home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </header>
+    </div>
+  );
+}
+
+export default App;
